f3d6a0f82061467d4ffd14599ee364f7e6d0def2
Jay Fanse <jayf29112003@gmail.com>
2025-01-27 17:19:48 +0530

    updated cors error

--- Server/config/express.js
+++ Server/config/express.js
@@ -14,6 +14,30 @@ const allowedOrigins = [
   process.env.CLIENT_BASE_URL_LIVE            // Second front-end URL (local development)
 ];
 
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (allowedOrigins.includes(origin) || !origin) { // Allow requests with no origin (e.g., Postman, curl)
+//         callback(null, true);
+//       } else {
+//         callback(new Error('CORS not allowed'), false);
+//       }
+//     },
+//     credentials: true, 
+//     // allowedHeaders: ['Content-Type', 'Authorization'],
+//   })
+// );
+
+
+// app.options('*', (req, res) => {
+//   res.setHeader('Access-Control-Allow-Origin', req.headers.origin || process.env.CLIENT_BASE_URL_LIVE);
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   res.status(204).send();
+// });
+
+
 app.use(
   cors({
     origin: (origin, callback) => {
@@ -24,18 +48,10 @@ app.use(
       }
     },
     credentials: true, 
-    // allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
   })
 );
 
-
-app.options('*', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || process.env.CLIENT_BASE_URL_LIVE);
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.status(204).send();
-});
  
 
 
