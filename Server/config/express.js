7427bb92a82f78248b6d9861a8ea9be7350336a1
unknown <sethtanmay72@gmail.com>
2025-01-28 12:00:28 +0530

    multiple server calling

--- Server/config/express.js
+++ Server/config/express.js
@@ -26,7 +26,9 @@ const backendInstances = [
   // "https://gyapak.vercel.app",
   // "https://gyapak-qngw.vercel.app",
   // "https://gyapak-tkpi.vercel.app",
-  "https://gyapak-2.onrender.com"
+  "https://gyapak-2.onrender.com",
+  "https://gyapak.onrender.com",
+  "https://gyapak-3.onrender.com"
 ];
 
 // Round-robin index
