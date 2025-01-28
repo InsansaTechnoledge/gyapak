e6b5cdd4c7a2127fbce10db08db122442d786cd5
Jay Fanse <jayf29112003@gmail.com>
2025-01-28 14:30:31 +0530

    updated config

--- insansagov/src/Pages/config.js
+++ insansagov/src/Pages/config.js
@@ -1,8 +1,10 @@
-const API_BASE_URL = [
-  "https://gyapak-2.onrender.com",
-  "https://gyapak.onrender.com",
-  "https://gyapak-3.onrender.com",
-];
+// const API_BASE_URL = [
+//   "https://gyapak-2.onrender.com",
+//   "https://gyapak.onrender.com",
+//   "https://gyapak-3.onrender.com",
+// ];
+
+const API_BASE_URL = "https://gyapak.onrender.com";
 
 // const API_BASE_URL = ["http://localhost:3000"];
 
