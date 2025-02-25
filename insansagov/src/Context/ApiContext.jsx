388e7f0dbb3154b8becc0b7a65c1e8f971b57648
Jay Fanse <jayf29112003@gmail.com>
2025-02-25 17:08:20 +0530

    caching added in navbar

--- insansagov/src/Context/ApiContext.jsx
+++ insansagov/src/Context/ApiContext.jsx
@@ -1,5 +1,7 @@
-import React,{createContext, useContext, useEffect, useState} from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import axios from 'axios';
+import { RingLoader } from 'react-spinners';
+import ErrorPage from '../Pages/Error/ErrorPage';
 
 
 const ApiContext = createContext();
@@ -13,26 +15,26 @@ export const SERVER_URLS = [
     "http://localhost:5001",
     "http://localhost:5002",
     "http://localhost:3000"
-  ];
-  
-export const ApiProvider=({children})=>{
-    const [apiBaseUrl,setApiBaseUrl]=useState(null);
-    const [loading,setLoading]=useState(true);
-    const [error,setError]=useState(null);
+];
 
-    useEffect(()=>{
-        async function CheckServer(){
-            for(let url of SERVER_URLS){
-                try{
-                    const response=await axios.get(`${url}`);
-                    if(response.data==="Server is running perfectly !!"){
+export const ApiProvider = ({ children }) => {
+    const [apiBaseUrl, setApiBaseUrl] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        async function CheckServer() {
+            for (let url of SERVER_URLS) {
+                try {
+                    const response = await axios.get(`${url}`);
+                    if (response.data === "Server is running perfectly !!") {
                         setApiBaseUrl(url);
                         setLoading(false);
-                        console.log("🚀 Using API:",url);
+                        console.log("🚀 Using API:", url);
                         return;
                     }
                 }
-                catch(error){
+                catch (error) {
                     console.warn(`❌ Failed: ${url}`, error.message);
                 }
             }
@@ -40,28 +42,31 @@ export const ApiProvider=({children})=>{
             setLoading(false);
         }
         CheckServer();
-    },[]);
+    }, []);
 
-    return(
-        <ApiContext.Provider value={{apiBaseUrl}}>
+    return (
+        <ApiContext.Provider value={{ apiBaseUrl }}>
             {loading
-            ?(
-            <h2>Checking API Availability...</h2>)
-            :error 
-            ?(
-            <h2>{error}</h2>)
-            :(
-            children)}
+                ? (
+                    <div className='w-full h-screen flex justify-center'>
+                    <RingLoader size={60} color={'#5B4BEA'} speedMultiplier={2} className='my-auto' />
+                </div>
+                )
+                : error
+                    ? (
+                       <ErrorPage code={503} message={"Oops! Looks like server has crashed :("} subMessage={"Please check your internet connection and reload the site."}/>)
+                    : (
+                        children)}
         </ApiContext.Provider>
     );
 
-    
+
 }
 
 export const useApi = () => {
     const context = useContext(ApiContext);
     if (!context) {
-      throw new Error("useApi must be used within an ApiProvider");
+        throw new Error("useApi must be used within an ApiProvider");
     }
     return context;
-  };
+};
