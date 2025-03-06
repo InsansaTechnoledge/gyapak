9282ccbb73039a7c8a1486e2a3bbb6a602d5e9d4
unknown <sethtanmay72@gmail.com>
2025-03-06 16:23:34 +0530

    dist add

---
+++ insansagov/dist/assets/ViewMoreButton-COQiTmiz.js
@@ -0,0 +1,6 @@
+import{c as n,j as e}from"./index-BKMSpVWi.js";import{C as s}from"./chevron-down-C27PXoqE.js";/**
+ * @license lucide-react v0.469.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=n("ChevronUp",[["path",{d:"m18 15-6-6-6 6",key:"153udz"}]]),p=({content:r,onClick:t})=>e.jsx("button",{onClick:t,className:"flex items-center gap-2 px-4 py-2 bg-purple-800 text-white rounded-md hover:bg-purple-900 transition duration-300",children:r.includes("▲")?e.jsxs(e.Fragment,{children:["View Less ",e.jsx(o,{})]}):e.jsxs(e.Fragment,{children:["View More ",e.jsx(s,{})]})});export{p as default};
