0113d2eb57f98cf3c4a6766e739fa3caabb3bbad
Tanmay seth <tanmayseth@Tanmays-MacBook-Air.local>
2025-03-18 19:02:58 +0530

    srg

--- insansagov/dist/assets/ScrollTopTo-DPKSPhyK.js
+++
@@ -1,1 +0,0 @@
-import{a as t,r as a}from"./index-BSoFxZP0.js";const s=()=>{const o=t();return a.useEffect(()=>{window.scrollTo(0,0)},[o.pathname]),null};export{s as default};
---
+++ insansagov/dist/assets/ScrollTopTo-HlhzT4c3.js
@@ -0,0 +1,1 @@
+import{a as t,r as a}from"./index-BFkRaJJ4.js";const s=()=>{const o=t();return a.useEffect(()=>{window.scrollTo(0,0)},[o.pathname]),null};export{s as default};
