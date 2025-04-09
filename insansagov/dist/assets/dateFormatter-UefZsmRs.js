9d31dc30a77e6f34f263ce070125cac229df2dc7
Tanmay seth <tanmayseth@Tanmays-MacBook-Air.local>
2025-04-09 17:45:32 +0530

    created user and pdf schema and api error and response

---
+++ insansagov/dist/assets/dateFormatter-UefZsmRs.js
@@ -0,0 +1,1 @@
+const a=e=>new Date(e).toLocaleDateString("en-GB",{day:"2-digit",month:"2-digit",year:"numeric"}).replace(",","").replaceAll("/","-");export{a as f};
---
+++ insansagov/dist/assets/no_data-ZhIxPc4_.js
@@ -0,0 +1,1 @@
+const a="/assets/no_data-ClaHdHxf.jpg";export{a as n};
