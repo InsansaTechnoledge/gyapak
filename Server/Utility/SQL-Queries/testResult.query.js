a534b7a63f5e023d2199910ddb1d93e77f2bcb78
Jay Fanse <jayf29112003@gmail.com>
2025-04-10 15:36:31 +0530

    test result wrong response store

--- Server/Utility/SQL-Queries/testResult.query.js
+++ Server/Utility/SQL-Queries/testResult.query.js
@@ -1,5 +1,14 @@
 import { supabase } from "../../config/supabaseClient";
 
-export const storeWrongResponses = async () => {
+export const storeWrongResponses = async (responses) => {
     
+    
+
+    const {data,error} = await supabase
+    .from('user_wrong_responses')
+    .insert(responses)
+    .select();
+
+    if(error) throw error;
+    return data;
 }
