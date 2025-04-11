b8d3906686b98bdf9f92efbfeca24cd14c2a90e8
Jay Fanse <jayf29112003@gmail.com>
2025-04-11 17:52:27 +0530

    responsive thoda thoda

--- gyapak-test-series/src/components/sections/landing/Hero/Hero.jsx
+++ gyapak-test-series/src/components/sections/landing/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import HeroImage from './HeroImage'
 const Hero = () => {
   return (
     <>
-      <div className='grid lg:grid-cols-2 gap-x-16 px-32 pt-28 pb-30 space-y-20 lg:space-y-0'>
+      <div className='grid lg:grid-cols-2 gap-x-16 px-12 md:px-32 pt-28 pb-30 space-y-20 lg:space-y-0'>
         <HeroContent />
         <HeroImage />  
       </div>
