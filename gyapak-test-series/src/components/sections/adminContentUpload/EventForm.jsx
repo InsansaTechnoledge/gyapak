2837a99a8219a24e7a76654a3b965208c369e073
Jay Fanse <jayf29112003@gmail.com>
2025-04-14 17:04:24 +0530

    frontend for exam form

---
+++ gyapak-test-series/src/components/sections/adminContentUpload/EventForm.jsx
@@ -0,0 +1,109 @@
+import React, { useEffect, useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
+
+const EventForm = ({ formData, setFormData }) => {
+    const [entries, setEntries] = useState([]);
+
+    const addEntry = () => {
+        setEntries([...entries, { id: uuidv4(), name: '', week: '', subjects: [] }]);
+    }
+
+    const handleChange = (id, field, value) => {
+        setEntries(entries.map((entry) => (entry.id === id ? { ...entry, [field]: value } : entry)));
+    };
+
+    const handleCheckboxChange = (id, field, value) => {
+        console.log(value);
+        setEntries(entries.map((entry) => {
+            if (entry.id === id) {
+                const currentValues = entry[field] || [];
+                const newValues = currentValues.includes(value)
+                    ? currentValues.filter((v) => v !== value) // Remove if already present (unchecked)
+                    : [...currentValues, value]; // Add if not present (checked)
+                
+                return { ...entry, [field]: newValues };
+            }
+            return entry;
+        }));
+    }
+
+
+    const deleteEntry = (id) => {
+        setEntries(entries.filter(entry => entry.id != id));
+    }
+
+    useEffect(() => {
+        setFormData(prev => ({
+            ...prev,
+            events: entries
+        }))
+    }, [entries])
+
+    return (
+        <div className="border-t pt-6 mt-6">
+            <h3 className="text-lg font-semibold mb-4">📆 Add Events</h3>
+            {
+                entries.map(({ id, name, week, subjects }) => (
+                    <div className="flex gap-4 mb-4">
+                        <input
+                            className="border p-2 w-full"
+                            placeholder="Event Name"
+                            value={name}
+                            onChange={(e) => handleChange(id, 'name', e.target.value)}
+                        />
+                        <input
+                            className="border p-2 w-full"
+                            placeholder="Week Number"
+                            value={week}
+                            onChange={(e) => handleChange(id, 'week', e.target.value)}
+                        />
+                        <div
+                            multiple
+                            className="border p-2 w-full flex"
+                            value={subjects}
+                        >
+                            {formData.subjects.map((s, i) => (
+                                <>
+                                    <input
+                                        key={id+s.id}
+                                        id={id+s.id}
+                                        name={'check'}
+                                        type='checkbox'
+                                        value={s.name}
+                                        onChange={(e)=>handleCheckboxChange(id, 'subjects', e.target.value)}
+                                        
+                                    />
+                                    <label htmlFor={id+s.id}>{s.name}</label>
+                                </>
+                            ))}
+                        </div>
+                        <button
+                            type="button"
+                            onClick={() => deleteEntry(id)}
+                        >
+                            delete
+                        </button>
+                    </div>
+                ))
+            }
+
+            <button
+                type='button'
+                onClick={addEntry}
+                className="bg-blue-500 text-white px-4 rounded"
+            >
+                Add
+            </button>
+
+            <ul className="list-disc ml-5 text-sm text-gray-700 mb-6">
+                {entries.map((e, i) => (
+                    <li key={i}>
+                        {e.name} — Week {e.week} — 📚 {e.subjects}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default EventForm
---
+++ gyapak-test-series/src/components/sections/adminContentUpload/SubjectForm.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
+
+const SubjectForm = ({setFormData}) => {
+    const [entries, setEntries] = useState([]);
+
+    const addEntry = () => {
+        setEntries([...entries, { id: uuidv4(), name: '', weightage: '', syllabus: null }]);
+    }
+
+    const handleChange = (id, field, value) => {
+        setEntries(entries.map((entry) => (entry.id === id ? { ...entry, [field]: value } : entry)));
+    };
+
+    const deleteEntry = (id) => {
+        setEntries(entries.filter(entry => entry.id != id));
+    }
+
+    useEffect(() => {
+        setFormData(prev => ({
+            ...prev,
+            subjects: entries
+        }));
+    },[entries])
+
+    return (
+        <div className="border-t pt-6 mt-6">
+            <h3 className="text-lg font-semibold mb-4">📚 Add Subjects</h3>
+
+            {
+                entries.map(({id, name, weightage, file}) => (
+                    <div key={id} className="flex gap-4 mb-4">
+                        <input
+                            className="border p-2 w-full"
+                            placeholder="Subject Name"
+                            value={name}
+                            onChange={(e) => (handleChange(id, 'name', e.target.value))}
+                        />
+                        <input
+                            type="number"
+                            className="border p-2 w-full"
+                            placeholder="Weightage (%)"
+                            value={weightage}
+                            onChange={(e) => (handleChange(id, 'weightage', e.target.value))}
+                        />
+                        <input
+                            type="file"
+                            onChange={(e) => (handleChange(id, 'syllabus', e.target.files[0]))}
+                        />
+                        <button 
+                        type='button'
+                        onClick={()=>deleteEntry(id)}>
+                            remove
+                        </button>
+                    </div>
+                ))
+            }
+            <button
+                type="button"
+                onClick={addEntry}
+                className="bg-blue-500 text-white px-4 rounded"
+            >
+                Add
+            </button>
+
+            <ul className="list-disc ml-5 text-sm text-gray-700 mb-6">
+                {entries.map((s, i) => (
+                    <li key={i}>
+                        {s.name} — {s.weightage}% — 📄 {s.syllabus?.name}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default SubjectForm
