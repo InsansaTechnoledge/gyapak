24a37b6326a37b23cda036a0afdc87ef5605600b
BHADRIKA <bhadrika2226@gmail.com>
2025-04-30 17:22:05 +0530

    Refactor event fetching logic and add event type selection in calendar view

--- Server/routes/eventRoutes.js
+++ Server/routes/eventRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express'
-import { getEvent, getEventsByCategory, getLatestUpdates, lastupdated } from '../controller/eventController.js';
+import { getEvent, getEventsForCalendar, getEventTypes, getLatestUpdates, lastupdated } from '../controller/eventController.js';
 
 const router = express.Router();
 
+router.get('/getEventTypes',getEventTypes);
 router.get('/latest', getLatestUpdates)
 router.get('/lastupdated', lastupdated);
-router.get('/', getEventsByCategory);
+router.get('/', getEventsForCalendar);
 router.get('/:eventId' ,getEvent);
+
 
 export default router;
