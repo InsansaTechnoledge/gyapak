37b4d104e71dd0b04c730576469e38803df1cb7a
Tanmay seth <tanmayseth@Tanmays-MacBook-Air.local>
2025-05-06 15:47:23 +0530

    result component added

--- insansagov/src/Components/Navbar/Navbar.jsx
+++ insansagov/src/Components/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const stateImages = {
         "Odisha": "/states/Odisha.png",
         "Punjab": "/states/Punjab.png",
         "Rajasthan": "/states/Rajasthan.png",
-        "Uttar Pradesh": "/states/Uttar_pradesh.png",
+        "Uttar Pradesh": "/states/UttarPradesh2.png",
         "Madhya Pradesh": "/states/Madhya Pradesh.png",
         "Tamil Nadu": "/states/Tamil_Nadu.png",
         "Uttarakhand": "/states/Uttarakhand.png",
