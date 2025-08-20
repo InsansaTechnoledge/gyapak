c052e600c1844238cec98b18177ffdba84ec55ff
harshvaidya04 <158769989+harshvaidya04@users.noreply.github.com>
2025-08-21 04:00:50 +0530

    fix: updated CORS configuration to include CloudFront URL and modify API base URL

--- adminserver/config/express.js
+++ adminserver/config/express.js
@@ -12,7 +12,7 @@ ExpressApp.use(express.urlencoded({ extended: true }));
 
 ExpressApp.use(
     cors({
-      origin: ['http://localhost:5174', 'http://localhost:5173', 'https://gyapak.in' , 'https://gyapak-admin-upload-data.vercel.app'],
+      origin: ['http://localhost:5174', 'http://localhost:5173', 'https://gyapak.in' , 'https://gyapak-admin-upload-data.vercel.app', 'https://d3paexwp5ivh8p.cloudfront.net'],
       credentials: true,
     })
   );
