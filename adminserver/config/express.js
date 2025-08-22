49c87c0fa22833acdf3037f2f42f032e94ef05f0
harshvaidya04 <158769989+harshvaidya04@users.noreply.github.com>
2025-08-22 12:58:19 +0530

    Update allowed CORS origins in Express config
    Replaced 'https://d2gcq65jmgagh2.cloudfront.net' with 'https://harshvaidya.tech' in the CORS origin whitelist to reflect updated domain access requirements.

--- adminserver/config/express.js
+++ adminserver/config/express.js
@@ -12,7 +12,7 @@ ExpressApp.use(express.urlencoded({ extended: true }));
 
 ExpressApp.use(
     cors({
-      origin: ['https://d2gcq65jmgagh2.cloudfront.net', 'http://localhost:5174', 'http://localhost:5173', 'https://gyapak.in' , 'https://gyapak-admin-upload-data.vercel.app'],
+      origin: ['https://harshvaidya.tech', 'http://localhost:5174', 'http://localhost:5173', 'https://gyapak.in' , 'https://gyapak-admin-upload-data.vercel.app'],
       credentials: true,
     })
   );
