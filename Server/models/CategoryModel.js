c023648986f14b0d9a2435088987f117df8e7f3c
harshvaidya04 <158769989+harshvaidya04@users.noreply.github.com>
2025-10-03 12:41:45 +0530

    fix: refactor image fetching logic to ensure base64 conversion only occurs for valid URLs

--- Server/models/CategoryModel.js
+++ Server/models/CategoryModel.js
@@ -3,14 +3,15 @@ import {DEFAULT_LOGO} from '../config/env.js';
 import fetch from 'node-fetch';
 const imageUrl = DEFAULT_LOGO;
 let base64String = "";
-try{
-    const response = await fetch(imageUrl);
-    const arrayBuffer = await response.arrayBuffer();
-    const imageBuffer = Buffer.from(arrayBuffer);
-
-     base64String = imageBuffer.toString('base64');
-}catch(error){
-    console.error('Error fetching image:', error);
+if (imageUrl && imageUrl.startsWith('http')) {
+    try{
+        const response = await fetch(imageUrl);
+        const arrayBuffer = await response.arrayBuffer();
+        const imageBuffer = Buffer.from(arrayBuffer);
+        base64String = imageBuffer.toString('base64');
+    }catch(error){
+        console.error('Error fetching image:', error);
+    }
 }
 
 const CategorySchema = new mongoose.Schema({
