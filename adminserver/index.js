273febb2c2a22cd9fdfa48be34ba2b44956010cf
Vishnu Vijayan <vishnupv312@gmail.com>
2025-12-15 14:23:03 +0530

    adde dthe scehduler for page : todays current affair, also added a cronjob for this to set it to published

--- adminserver/index.js
+++ adminserver/index.js
@@ -1,10 +1,11 @@
-import dotenv from 'dotenv';
-import http from 'http';
-import { Server as SocketIOServer } from 'socket.io';
-import cron from 'node-cron';
-import { monitorAllSources } from './Jobs/monitorSources.job.js';
+import dotenv from "dotenv";
+import http from "http";
+import { Server as SocketIOServer } from "socket.io";
+import cron from "node-cron";
+import { monitorAllSources } from "./Jobs/monitorSources.job.js";
+import publishScheduledPdfs from "./Jobs/scheduled-magazine.job.js";
 
-import  {File, Blob} from 'node:buffer';
+import { File, Blob } from "node:buffer";
 
 globalThis.File = File;
 globalThis.Blob = Blob;
@@ -14,17 +15,17 @@ globalThis.Blob = Blob;
 dotenv.config();
 
 // dynamic import of app (keeps your current pattern)
-const app = (await import('./app.js')).default;
+const app = (await import("./app.js")).default;
 
 const initialize = async () => {
   try {
     const appInstance = await app();
 
     // Health check route (keep this)
-    appInstance.get('/', (req, res) => {
-      console.log('server is live ');
+    appInstance.get("/", (req, res) => {
+      console.log("server is live ");
       res.status(200).json({
-        message: 'server is running',
+        message: "server is running",
       });
     });
 
@@ -35,31 +36,31 @@ const initialize = async () => {
     const io = new SocketIOServer(server, {
       cors: {
         origin: [
-          'https://d3bxc62b4f5pj5.cloudfront.net',
-          'http://localhost:5174',
-          'http://localhost:5173',
-          'https://gyapak.in',
-          'https://www.gyapak.in',
-          'https://gyapak-admin-upload-data.vercel.app',
-          'http://localhost:5176',
-          'http://localhost:5175',
+          "https://d3bxc62b4f5pj5.cloudfront.net",
+          "http://localhost:5174",
+          "http://localhost:5173",
+          "https://gyapak.in",
+          "https://www.gyapak.in",
+          "https://gyapak-admin-upload-data.vercel.app",
+          "http://localhost:5176",
+          "http://localhost:5175",
         ],
         credentials: true,
       },
     });
 
     // 🔹 3) Handle Socket.IO connections
-    io.on('connection', (socket) => {
-      console.log('Client connected:', socket.id);
+    io.on("connection", (socket) => {
+      console.log("Client connected:", socket.id);
 
       // Frontend will send: socket.emit("subscribe-source", "CENTRAL_UPSC")
-      socket.on('subscribe-source', (sourceCode) => {
+      socket.on("subscribe-source", (sourceCode) => {
         socket.join(sourceCode);
         console.log(`Socket ${socket.id} joined room ${sourceCode}`);
       });
 
-      socket.on('disconnect', () => {
-        console.log('Client disconnected:', socket.id);
+      socket.on("disconnect", () => {
+        console.log("Client disconnected:", socket.id);
       });
     });
 
@@ -67,19 +68,25 @@ const initialize = async () => {
     global.io = io;
 
     // 🔹 4) Cron job: monitor ALL sources every 2 minutes
-    cron.schedule('*/2 * * * *', async () => {
-      console.log('Cron: running monitorAllSources...');
+    cron.schedule("*/2 * * * *", async () => {
+      console.log("Cron: running monitorAllSources...");
       await monitorAllSources(io);
     });
 
-    // 🔹 5) Start HTTP server (NOT appInstance.listen anymore)
+    // 🔹 5) Cron job: publish scheduled PDFs every 30 minutes
+    cron.schedule("*/1 * * * *", async () => {
+      console.log("Cron: running publishScheduledPdfs...");
+      await publishScheduledPdfs();
+    });
+
+    // 🔹 6) Start HTTP server (NOT appInstance.listen anymore)
     const PORT = process.env.PORT || 5000;
 
     server.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   } catch (err) {
-    console.log(err, 'There was an error initializing the server');
+    console.log(err, "There was an error initializing the server");
   }
 };
 
