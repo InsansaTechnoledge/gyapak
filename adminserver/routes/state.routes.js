2f63a42aaef5681a222c3b58a9eef13e3f7d3087
Vishnu Vijayan <vishnupv312@gmail.com>
2025-12-19 15:14:24 +0530

    Added the route to add the state in the database and added 2 state i.e west bengal and delhi on dev server

--- adminserver/routes/state.routes.js
+++ adminserver/routes/state.routes.js
@@ -1,8 +1,13 @@
-import express from 'express'
-import { getStateList } from '../controllers/state.controller.js';
+import express from "express";
+import {
+  getStateList,
+  addState,
+  upload,
+} from "../controllers/state.controller.js";
 
 const router = express.Router();
 
-router.get('/all', getStateList);
+router.get("/all", getStateList);
+router.post("/add-state", upload.single("logo"), addState);
 
 export default router;
